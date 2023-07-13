375b2672498158ee63a03776a55992fbff9ff09f
AndreaFerrara13 <134604849+AndreaFerrara13@users.noreply.github.com>
2023-07-13 16:42:24 +0200

    es

---
+++ js.js
@@ -0,0 +1,11 @@
+const estrai = function () {
+  const number = Math.round(Math.random() * 76);
+  console.log(number);
+};
+estrai();
+const cell = [];
+const numeri = function () {
+  for (let i = 0; i < 76; i++);
+  cell.push = [];
+};
+numeri();
