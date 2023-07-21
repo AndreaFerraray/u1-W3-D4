0d44576af7ffc7ed950901646a78df1df33ecf70
AndreaFerrara13 <134604849+AndreaFerrara13@users.noreply.github.com>
2023-07-21 15:39:07 +0200

    numeri cartella

--- index.js
+++ index.js
@@ -9,12 +9,42 @@ const creaTabellone = function () {
 creaTabellone();
 
 // ------------------------------------------------------------------------------
+const arrayNum = [];
+const arrayGiocatore = [];
+const numRandom = function () {
+  const numIndex = Math.floor(Math.random() * arrayNum.length);
 
-const numRandom = function (min, max) {
-  const numIndex = Math.floor(Math.random() * min);
-  const random = range.splice(numIndex, 1)[0];
+  const random = arrayNum.splice(numIndex, 1);
+  console.log(random);
   return random;
 };
-let numero = [];
-numero.push(numRandom(1, 76));
-console.log(numero);
+const Array = function () {
+  for (let i = 0; i < 76; i++) {
+    arrayNum.push(i + 1);
+  }
+  arrayGiocatore = arrayNum.slice();
+};
+Array();
+
+const estrazione = function () {
+  const numeroEstratto = numRandom();
+  const celle = document.querySelectorAll(".cell");
+  celle[numeroEstratto - 1].classList.add("onCasella");
+  console.log(arrayNum.length);
+};
+const button = document.querySelector(".estrai");
+button.addEventListener("click", estrazione);
+
+const estraiCartella = document.querySelector("button:nth-of-type(2)");
+
+const cartellina = [];
+//qui otteniammo i numeri random che compongono la cartella
+const buttonEstrai = () => {
+  for (let i = 0; i < 24; i++) {
+    const x = Math.floor(Math.random() * arrayGiocatore.length);
+    cartellina.push(arrayGiocatore[x]);
+    arrayGiocatore.splice(x, 1);
+  }
+};
+//devo creare il div contenente i div con i numeri della cartella
+estraiCartella.addEventListener("click", buttonEstrai);
